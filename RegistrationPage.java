d9bf2fac8dcb47bd4b2730e90b975a064178f1ca
Tanoy <107623581+TanoyGit@users.noreply.github.com>
2025-08-09 20:31:07 +0530

    Update RegistrationPage.java

--- RegistrationPage.java
+++ RegistrationPage.java
@@ -1,54 +1,58 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+package CapStone.EcommercePrj;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.time.Duration;
 
-public class RegistrationTest {
-    public static void main(String[] args) {
-        // Set up WebDriver
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        
-        // Implicit wait - applies to all element finds
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-        try {
-            // Open the site
-            driver.get("https://www.demoblaze.com/"); // change to your site URL
+public class RegistrationPage {
 
-            // Click Sign up link
-            driver.findElement(By.linkText("Sign up")).click();
 
-            // Enter username
-            driver.findElement(By.id("sign-username")).sendKeys("testUser");
+	    public static void main(String[] args) {
+	        
+	        WebDriver driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	        
+	       
+	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-            // Enter password
-            driver.findElement(By.id("sign-password")).sendKeys("testPass123");
+	        try {
+	           
+	            driver.get("https://www.demoblaze.com/"); // change to your site URL
 
-            // Click Sign up button
-            driver.findElement(By.xpath("//button[@onClick='register()']")).click();
+	            
+	            driver.findElement(By.linkText("Sign up")).click();
 
-            // Take screenshot of entire screen
-            String timestamp = String.valueOf(System.currentTimeMillis());
-            Robot robot = new Robot();
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            Rectangle screenRect = new Rectangle(0, 0, screenSize.width, screenSize.height);
-            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-            File dest = new File(System.getProperty("user.dir") + "/screenshots/Registration_" + timestamp + ".png");
-            ImageIO.write(screenFullImage, "png", dest);
+	            
+	            driver.findElement(By.id("sign-username")).sendKeys("testUser");
 
-            System.out.println("Screenshot saved: " + dest.getAbsolutePath());
+	            
+	            driver.findElement(By.id("sign-password")).sendKeys("testPass123");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Close the browser
-            driver.quit();
-        }
-    }
-}
+	            
+	            driver.findElement(By.xpath("//button[@onClick='register()']")).click();
+
+	           //Screenshot
+	            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+                //Save Screenshot
+	            File destination = new File(System.getProperty("Desktop") + "/screenshotsr/page.png");
+	            destination.getParentFile().mkdirs(); 
+	            FileUtils.copyFile(screenshot, destination);
+
+	            System.out.println("Screenshot saved at: " + destination.getAbsolutePath());
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	           
+	            driver.quit();
+	        }
+	    }
+	}
