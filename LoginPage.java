97fb7300d4b63a8275b33fea0b38b7652630be91
Tanoy <107623581+TanoyGit@users.noreply.github.com>
2025-08-10 09:54:52 +0530

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -1,92 +1,34 @@
-package Pages;
+package CapStone.EcommercePrj;
 
-import org.apache.commons.io.FileUtils;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+public class LoginPg {
 
-public class LoginPage {
-    private final WebDriver driver;
-    Boolean flag = false;
-    TakesScreenshot ts;
+	public static void main(String[] args) {
+	
+		
+        WebDriver driver = new ChromeDriver();
+        
+        driver.manage().window().maximize();
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        driver.get("https://www.demoblaze.com/");
+        
+        driver.findElement(By.id("login2")).click(); 
+        
+        driver.findElement(By.id("loginusername")).sendKeys("testUser"); 
+        
+        driver.findElement(By.id("loginpassword")).sendKeys("testPass123"); 
+        
+        driver.findElement(By.xpath("//button[contains(text(),'Log in')]")).click();
+        
+        driver.close();
 
-    public LoginPage(WebDriver driver) {
-        this.driver = driver;
-    }
+	}
 
-    private final By loginLink = By.linkText("Log in");
-    private final By username = By.id("loginusername");
-    private final By password = By.id("loginpassword");
-    private final By loginButton = By.xpath("//button[@onclick='logIn()']");
-    private final By userButton = By.id("nameofuser");
-
-
-    public void clickLoginLink() {
-        driver.findElement(loginLink).click();
-    }
-
-    public void enterUsername(String uname) {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        driver.findElement(username).sendKeys(uname);
-    }
-
-    public void enterPassword(String pass) {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        driver.findElement(password).sendKeys(pass);
-    }
-
-    public void clickLoginButton() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        driver.findElement(loginButton).click();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public boolean isLoginSuccessful() {
-        if (driver.findElement(userButton).isDisplayed()) {
-            flag = true;
-        }
-        return flag;
-    }
-    public String takeScreenshot() {
-        String className = this.getClass().getSimpleName();
-        String timestamp = String.valueOf(System.currentTimeMillis());
-        String relativePath = "./screenshots/" + className + "_" + timestamp + ".png";
-        try {
-
-            Robot robot = new Robot();
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            Rectangle screenRect = new Rectangle(0, 0, screenSize.width, screenSize.height);
-            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-
-            relativePath = "./screenshots/" + className + "_" + timestamp + ".png";
-            File dest = new File(System.getProperty("user.dir") + "/screenshots/" + className + "_" + timestamp + ".png");
-            ImageIO.write(screenFullImage, "png", dest);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return relativePath;
-    }
 }
