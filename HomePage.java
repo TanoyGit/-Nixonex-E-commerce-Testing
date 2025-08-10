6e17ccb9bbe2429b742326f1e8bd94b58daf854b
Tanoy <107623581+TanoyGit@users.noreply.github.com>
2025-08-10 10:14:05 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,57 +1,47 @@
 package CapStone.EcommercePrj;
 
-
-import java.io.File;
 import java.time.Duration;
+import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RegistrationPage {
+public class HomePage {
+    public static void main(String[] args) {
+        
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        try {
+            
+            driver.get("https://www.demoblaze.com/"); 
 
 
-	    public static void main(String[] args) {
-	        
-	        WebDriver driver = new ChromeDriver();
-	        driver.manage().window().maximize();
-	        
-	       
-	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-	        try {
-	           
-	            driver.get("https://www.demoblaze.com/"); 
+            driver.findElement(By.xpath("//a[contains(text(),'Laptops')]")).click(); 
+            
+            driver.findElement(By.linkText("MacBook Pro")).click(); 
+            
+            List <WebElement> els = driver.findElements(By.xpath("//div[@id='more-information']"));
+            
+           
+            for(WebElement e : els) {
+            	
+            	System.out.println(e.getText());
+            	
+            }
+               
 
-	            
-	            driver.findElement(By.linkText("Sign up")).click();
 
-	            
-	            driver.findElement(By.id("sign-username")).sendKeys("testUser");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+        
+    }
+}
 
-	            
-	            driver.findElement(By.id("sign-password")).sendKeys("testPass123");
-
-	            
-	            driver.findElement(By.xpath("//button[@onClick='register()']")).click();
-
-	           
-	            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-	            
-	            File destination = new File(System.getProperty("Desktop") + "/screenshotsr/page.png");
-	            destination.getParentFile().mkdirs(); 
-	            FileUtils.copyFile(screenshot, destination);
-
-	            System.out.println("Screenshot saved at: " + destination.getAbsolutePath());
-
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        } finally {
-	           
-	            driver.quit();
-	        }
-	    }
-	}
